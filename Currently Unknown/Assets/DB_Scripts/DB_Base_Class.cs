74b037fe6ffbcdffb46ff489cb73db6fa4a62223
davebrooks199909@gmail.com <davebrooks199909@gmail.com>
2019-02-19 02:38:04 +0000

    Second Addition Movement and Animations
    After struggling to see what i was doing with a basic PC using unitys Capsule and Cubes as arms i decided to find some animations for movement and fighting. Using Blend tress to smoothen the animations. There is now some realistic movement and also still achieving the attitude of only moving straight in a ring. No rotation is allowed yet. i would like to keep it the way it currently is as some of the animations spin the player slightly.
    
    Another day i will be making sure the Player can be forced back which will also work on NPCs. Forcing the player back also means adding a Head Hit animation and body hit animation. for these will be the two main attacks.
    
    There will also be an illegal elbow which will be looked at much later and adding a referee to allow players to consider choice and a thinking process to see if using the elbow is a good idea.

--- Currently Unknown/Assets/DB_Scripts/DB_Base_Class.cs
+++ Currently Unknown/Assets/DB_Scripts/DB_Base_Class.cs
@@ -23,26 +23,45 @@ using UnityEngine;
 public class DB_Base_Class : MonoBehaviour
 {
     [SerializeField]
-    private BoxCollider PC_RightHand_BC;
+    protected SphereCollider PC_RightHand_SC;
     [SerializeField]
-    private BoxCollider PC_LightHand_BC;
-    private CharacterController PC_CC;
-    protected float Speed = 5;
+    protected SphereCollider PC_LeftHand_SC;
+    [SerializeField]
+    protected CharacterController PC_CC;
+    [SerializeField]
+    protected float speed = 5;
+    [SerializeField]
+    protected float Side_step = 3;
+    [SerializeField]
+    protected Vector3 moveDirection = Vector3.zero;
     [SerializeField]
     private GameObject PC_Eyes_Camera;
-    [SerializeField]
+    [SerializeField]    // Remove
     protected bool canMove = true;
+    [SerializeField]    // Remove
+    private Animator anim;
+
+
     // Start is called before the first frame update
     protected virtual void Start()
     {
-        // Add IDE components 
-        PC_RightHand_BC = gameObject.transform.FindChild("Arm_right/Hand_right").gameObject.AddComponent<BoxCollider>();// Add BoxCollider on a child object
-        PC_RightHand_BC.size = new Vector3(1, 1, 0.16f);    // resize BoxCollider
-        PC_LightHand_BC = gameObject.transform.FindChild("Arm_left/Hand_left").gameObject.AddComponent<BoxCollider>();// Add BoxCollider on a child object
-        PC_LightHand_BC.size = new Vector3(1, 1, 0.16f);    // Resize Boxcollider
-        PC_RightHand_BC.isTrigger = false;
-        PC_LightHand_BC.isTrigger = false;
+        // Add IDE Components
+        #region Right Hand Size
+        PC_RightHand_SC = gameObject.transform.FindChild("mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:RightShoulder/mixamorig:RightArm/mixamorig:RightForeArm/mixamorig:RightHand").gameObject.AddComponent<SphereCollider>();// Add BoxCollider on a child object
+        PC_RightHand_SC.center = new Vector3(0.09f, 0, 0.01f);    // resize SphereCollider
+        PC_RightHand_SC.radius = 0.06f; // Set Sphere collider Radius
+        #endregion
+        #region Left Hand Size
+        PC_LeftHand_SC = gameObject.transform.FindChild("mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:LeftShoulder/mixamorig:LeftArm/mixamorig:LeftForeArm/mixamorig:LeftHand").gameObject.AddComponent<SphereCollider>();// Add BoxCollider on a child object
+        PC_LeftHand_SC.center = new Vector3(-0.07f, 0, 0.01f);    // Resize SphereCollider
+        PC_LeftHand_SC.radius = 0.07f;
+        #endregion
+        #region Character Controller
         PC_CC = gameObject.AddComponent<CharacterController>();     // Adds a character controller componenet
+        PC_CC.center = new Vector3(0, 1f, 0);
+        #endregion
+
+        anim = gameObject.GetComponent<Animator>();
 
     }
 
@@ -51,36 +70,78 @@ public class DB_Base_Class : MonoBehaviour
     {
         // Call Functions
         Movement();
+        Melee_Combat();
     }
 
     protected virtual void Movement()
     {
-        // Adding input to the movement Vector
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        PC_CC.Move(move * Time.deltaTime * Speed);
-        
+        // Allow speed float to hold input on keyboard forward and backwards
+        speed = Input.GetAxis("Vertical");  // Speed takes into the account of the input needed to move
+        anim.SetFloat("Speed", speed);      // Value in script of speed is now the animation float value of speed
+        PC_CC.Move(transform.forward * Time.deltaTime * speed);     // Monitor speed when we move and so we know how fast we move
+
+        // if the A, D, Left Arrow or Right Arrow are pushed down well a Animator Parameter boolean sets to true
+        if (Input.GetKeyDown(KeyCode.A) | Input.GetKeyDown(KeyCode.D) | Input.GetKeyDown(KeyCode.LeftArrow) | Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // The gameObjects animator in dervied classes finds boolean parameter and sets it true
+            anim.SetBool("SideStrife", true);
+        }
+        // However any of the A, D, Left Arrowor Right Arrow are no longer held down
+        else if(Input.GetKeyUp(KeyCode.A) | Input.GetKeyUp(KeyCode.D) | Input.GetKeyUp(KeyCode.LeftArrow) | Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            // parameter animator boolean is not true and new animation plays depending on what you set next
+            anim.SetBool("SideStrife", false);
+        }
+        // the Side step float carries the information for player input
+        Side_step = Input.GetAxis("Horizontal");
+        // Allow for animator parameter to use side step so blend tree knows when to change animation left = -1 | idle = 0 | right = 1
+        anim.SetFloat("Side Speed", Side_step);
+        // Move the Character with the character controller 
+        PC_CC.Move(transform.right * Time.deltaTime * Side_step);
+
         
 
-        // This can be removed when a better and smoother system can be made
-        //if (move != Vector3.zero)
-        //    transform.forward = move;
-
-        
     }
-
+    #region
+    // This function is used for when NPC and PC are in combat
+    // Players can input a key (Space Bar) and deliver a jab (This will be showed different in the NPC_Fighter Dervied Class)
+    // However players can use the base class version in the Player Controller
+    //The Function is allows players and NPC to push eachother back with force.
+    #endregion
     protected virtual void Melee_Combat()
     {
-
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("Jabbing", true);
+        }
+        else
+        {
+            if(Input.GetButtonUp("Jump"))
+            {
+                anim.SetBool("Jabbing", false);
+            }
+        }
+        
     }
-
+    #region
+    // This function allows NPC or Player to pick up an object to use in a fight
+    // However might be taken away and instead use power up pickups
+    #endregion
     protected virtual void PickUp_Weapon()
     {
 
     }
 
-    // Use this for melee combat however dont allow the player to see meshs clipping through eachother
-    protected virtual void OnTriggerEnter(Collider other)
+    protected virtual void StaminaBar()
     {
-        
+
+    }
+
+    protected virtual void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Hit");
+        }
     }
 }
--- Currently Unknown/Assets/DB_Scripts/DB_PC_Controller.cs
+++ Currently Unknown/Assets/DB_Scripts/DB_PC_Controller.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DB_PC_Controller : DB_Base_Class
 {
     public Transform NPC_target;
-    public float rotaionalSpeed = 10;
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -15,17 +14,16 @@ public class DB_PC_Controller : DB_Base_Class
     // Update is called once per frame
     protected override void Update()
     {
-        // Keep PC within a distance Cant get to close the NPC
-        Debug.DrawLine(NPC_target.position, transform.position, Color.yellow);  // Draws line between the Player and the NPC opponent
-        if(canMove)
-        {
-            if (Vector3.Distance(transform.position, NPC_target.position) < 2)
-            {
-                canMove = false;
-                transform.position = Vector3.MoveTowards(transform.position, NPC_target.position, Speed * Time.deltaTime);
-            }
-        }
-        
         base.Update();
+    }
+
+    protected override void Melee_Combat()
+    {
+        base.Melee_Combat();
+    }
+
+    protected override void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        base.OnControllerColliderHit(hit);
     }
 }
