786201e05391f75b0937fc6f0a300a15ac03c82d
davebrooks199909@gmail.com <davebrooks199909@gmail.com>
2019-02-25 21:30:25 +0000

    Commit 3 Camera Functionality
    I decided i wanted to see what i could do with the camera and focus the camera between 2 objects the PC and NPC so when fighting the camera had an effect to zoom in when the 2 fighters were close and zoom out when they were not.
    
    Also allowing for so more combat options and thinking about how i will be using my stamina system to effect the amount of force applied in every type of punch.

--- Currently Unknown/Assets/DB_Scripts/Base Class/DB_Base_Class.cs
+++ Currently Unknown/Assets/DB_Scripts/Base Class/DB_Base_Class.cs
@@ -34,8 +34,6 @@ public class DB_Base_Class : MonoBehaviour
     protected float Side_step = 3;
     [SerializeField]
     protected Vector3 moveDirection = Vector3.zero;
-    [SerializeField]
-    private GameObject PC_Eyes_Camera;
     [SerializeField]    // Remove
     protected bool canMove = true;
     [SerializeField]    // Remove
@@ -43,6 +41,20 @@ public class DB_Base_Class : MonoBehaviour
     [SerializeField]
     protected bool bodyPunch = false;
 
+
+    #region Camera Variables 
+    public List<Transform> targets;
+    public Vector3 offset;
+    public float smoothTime = .5f;
+
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    protected Camera cam;
+
+    private Vector3 velocity;
+    #endregion
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -75,6 +87,69 @@ public class DB_Base_Class : MonoBehaviour
         Melee_Combat();
     }
 
+    #region Camera Functions
+    #region LateUpdate Function
+    protected virtual void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        CameraMove();
+        Zoom();
+    }
+    #endregion
+
+    #region Camera Move
+    void CameraMove()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+    #endregion
+
+    #region Zoom Function
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    #endregion
+
+    #region Greatest Distance Functions
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+    #endregion
+
+    #region CenterPoint Function
+    protected virtual Vector3 GetCenterPoint()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+    #endregion
+    #endregion
+
+    #region Movement Function
     protected virtual void Movement()
     {
         // Allow speed float to hold input on keyboard forward and backwards
@@ -100,18 +175,21 @@ public class DB_Base_Class : MonoBehaviour
         anim.SetFloat("Side Speed", Side_step);
         // Move the Character with the character controller 
         PC_CC.Move(transform.right * Time.deltaTime * Side_step);
-
-        
-
     }
-    #region
+    #endregion
+
+    #region Notes
     // This function is used for when NPC and PC are in combat
     // Players can input a key (Space Bar) and deliver a jab (This will be showed different in the NPC_Fighter Dervied Class)
     // However players can use the base class version in the Player Controller
     //The Function is allows players and NPC to push eachother back with force.
     #endregion
+    
+    #region Melee Combat Function
     protected virtual void Melee_Combat()
     {
+        // List of different attacks via animations
+        // Change this button later
         if (Input.GetButtonDown("Jump"))
         {
             anim.SetBool("Jabbing", true);
@@ -123,7 +201,7 @@ public class DB_Base_Class : MonoBehaviour
                 anim.SetBool("Jabbing", false);
             }
         }
-
+        // Change this button later
         if(Input.GetButtonDown("Fire1"))
         {
             anim.SetBool("Body Punch", true);
@@ -135,22 +213,32 @@ public class DB_Base_Class : MonoBehaviour
                 anim.SetBool("Body Punch", false);
             }
         }
-        
     }
+    #endregion
+
     #region
     // This function allows NPC or Player to pick up an object to use in a fight
     // However might be taken away and instead use power up pickups
     #endregion
+    #region PickUp Weapon
     protected virtual void PickUp_Weapon()
     {
 
     }
-
+    #endregion
+    #region Stamina Monitor Function
+    // This function will be used to monitor how much energy is left in the fighter
+    // PC and NPC figters will have this bar using a UI slider to visually show it.
+    // More stamina you have the more power in your punch
+    // No stamina means you cant move and your punches are pointless 
+    // It leaves more strategic thinking on the players behalf
+    #endregion
+    #region Stamina Monitor Function
     protected virtual void StaminaBar()
     {
 
     }
-
+    #endregion
     protected virtual void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.gameObject.tag == "Enemy")
---
+++ Currently Unknown/Assets/DB_Scripts/Derived Class/DB_Camera_Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class DB_Camera_Movement : DB_Base_Class
+{
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+
+    protected override Vector3 GetCenterPoint()
+    {
+        return base.GetCenterPoint();
+    }
+}
